569cbf73a9eb89613a4cde7df53f8c25f90484c1
Lopatin Bohdan <lopatin.bogdan77@gmail.com>
2022-06-22 02:06:43 +0300

    adaptive design

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { ButtonModule } from 'primeng/button';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { Routes } from '@angular/router';
+import { ProductsListComponent } from 'libs/products/src/lib/pages/products-list/products-list.component';
+
+const routes: Routes = [
+    {
+        path: 'products',
+        component: ProductsListComponent
+    }
+];
 
 @NgModule({
     imports: [CommonModule, ButtonModule],
